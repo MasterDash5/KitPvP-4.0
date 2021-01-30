8f07fa55b5d25beef6154685e849d0945fdd9c29
MM5 <matthewmolmig@gmail.com>
2021-01-30 14:53:59 -0500

    frick weather all my homies hate weather

--- src/dashnetwork/kitpvp/listeners/WeatherListener.java
+++ src/dashnetwork/kitpvp/listeners/WeatherListener.java
@@ -1,6 +1,5 @@
 package dashnetwork.kitpvp.listeners;
 
-import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.weather.WeatherChangeEvent;
@@ -9,11 +8,7 @@ public class WeatherListener implements Listener {
 
     @EventHandler
     public void onWeatherChange(WeatherChangeEvent event) {
-        event.setCancelled(true);
-
-        World world = event.getWorld();
-
-        if (world.hasStorm())
-            world.setStorm(false);
+        if (event.toWeatherState())
+            event.setCancelled(true);
     }
 }
